01ae3826269e06a74ca111bc8a464e3625c73917
Omer Ahmed <omer1@ualberta.ca>
2016-02-19 16:56:59 -0700

    Classes
    Added classes for Inventory, Item, and Plant

---
+++ Har VS. Moon/Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+	List<Item> items = new List<Item>(10);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool AddItem(Item addedItem){
+		//if(
+
+		//if (items.Count () >= 10) {
+		//	return false;
+		//}else{
+			
+	}
+}
---
+++ Har VS. Moon/Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	//Item types: seed, harvested
+	public string itemType = "seed";
+	public string species = "light";
+	public int quantity = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool compareItems(Item itemToCompare){
+		
+	}
+}
