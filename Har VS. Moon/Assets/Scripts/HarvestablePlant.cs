bea312cc621e0be6f8f5adf764d438072bf6f5c9
Thomas Curnow <curnow@ualberta.ca>
2016-02-20 13:06:05 -0700

    Added plant harvesting

---
+++ Har VS. Moon/Assets/Scripts/HarvestablePlant.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarvestablePlant : MonoBehaviour {
+
+    GameObject parent;
+    PlantObject plant;
+	// Use this for initialization
+	void Start () {
+
+        parent = transform.parent.gameObject;
+        plant = parent.GetComponent<PlantObject>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("hello");
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("hello");
+            if (plant.age > plant.harvestTime)
+            {
+                plant.stage4.SetActive(false);
+                plant.item.SetActive(true);
+            }
+        }
+
+    }
+
+}
