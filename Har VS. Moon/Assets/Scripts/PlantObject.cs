2e9989e3eb999b77931fedbbd000d9f46daddc39
Thomas Curnow <curnow@ualberta.ca>
2016-02-19 19:46:33 -0700

    Added plant growing

---
+++ Har VS. Moon/Assets/Scripts/PlantObject.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlantObject : MonoBehaviour {
+
+    public int id;
+    public int stages;
+    public int hp;
+    public int hunger;
+    public int age;
+    public int currentStage;
+    public GameObject currentPlantObject;
+    //public List<GameObject> plantObjects = new List<GameObject>();
+
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
+    public GameObject stage4;
+    //public GameObject stage5;
+
+    public int growTime1;
+    public int growTime2;
+    public int growTime3;
+
+    public WorldManager worldManager;
+
+    public float timer;
+
+    bool change = true;
+    
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        stage2.SetActive(false);
+        stage3.SetActive(false);
+        stage4.SetActive(false);
+
+        worldManager = GameObject.Find("World").GetComponent<WorldManager>();
+	}
+	
+	// Update is called once per frame
+    //super hacky
+	void Update () {
+        timer += worldManager.getDeltaTime(); 
+        if ((timer > 10.0f)) 
+        {
+            grow();
+            timer = 0;
+            
+        }
+	}
+
+    void grow()
+    {
+        age = age + 1;
+        hunger = hunger - 1;
+
+        if (age == growTime1)
+        {
+            currentPlantObject = stage2;
+
+            stage1.SetActive(false);
+            stage2.SetActive(true);
+            stage3.SetActive(false);
+            stage4.SetActive(false);
+        }
+        if (age == growTime2)
+        {
+            currentPlantObject = stage3;
+            stage1.SetActive(false);
+            stage2.SetActive(false);
+            stage3.SetActive(true);
+            stage4.SetActive(false);
+        }
+        if (age == growTime3)
+        {
+            currentPlantObject = stage4;
+            stage1.SetActive(false);
+            stage2.SetActive(false);
+            stage3.SetActive(false);
+            stage4.SetActive(true);
+        }
+
+    }
+
+    /*
+    void insertPlantObject(int index, GameObject gameObject)
+    {
+        plantObjects.Insert(index, gameObject);
+    }
+
+    void addPlantObject(int index, GameObject gameObject)
+    {
+        plantObjects.Add(gameObject);
+    }
+    */
+
+}
---
+++ Har VS. Moon/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+
+    public float time;
+    public float speed; 
+
+	// Use this for initialization
+	void Start () {
+        //time += Time.deltaTime;
+        //speed = 1;
+
+        
+	}
+
+    void FixedUpdate(){
+        time += (Time.deltaTime * speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public float getTime()
+    {
+        return Mathf.Round(time);
+    }
+
+    public float getDeltaTime()
+    {
+        return (Time.deltaTime * speed);
+    }
+
+}
