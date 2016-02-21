d9cbcd9e31e2e5b662bc38e01ed5bd19048ab563
Thomas Curnow <curnow@ualberta.ca>
2016-02-20 21:20:13 -0700

    Added purchasing stub

---
+++ Har VS. Moon/Assets/Scripts/BuyScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuyScript : MonoBehaviour {
+
+    public GameObject worldObject;
+    public WorldManager worldManager;
+
+    public int cost;
+    public string type;
+
+	// Use this for initialization
+	void Start () {
+
+        worldObject = GameObject.Find("World");
+        if (worldObject == null)
+        {
+            Debug.LogError("World not found!");
+        }
+        worldManager = worldObject.GetComponent<WorldManager>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        if (worldManager.cheese > cost)
+        {
+            worldManager.cheese = worldManager.cheese - cost;
+        }
+    }
+}
