2a85375f985bf95864cd27f8b4365212dbc8cf0a
Thomas Curnow <curnow@ualberta.ca>
2016-02-21 10:46:14 -0700

    Added start button

---
+++ Har VS. Moon/Assets/Scripts/StartScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+    public GameObject gridObject;
+    public GridManager gridManager;
+
+	// Use this for initialization
+	void Start () {
+
+        gridObject = GameObject.Find("GridObject");
+        if (gridObject == null)
+        {
+            Debug.LogError("grid not found!");
+        }
+        gridManager = gridObject.GetComponent<GridManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void OnMouseUp()
+    {
+        Debug.Log("start!");
+        //if (Input.GetMouseButtonDown(1) == true)
+       // {
+            gridManager.createGrid();
+            gridManager.drawGrid();
+            Destroy(gameObject);
+       // }
+    }
+}
