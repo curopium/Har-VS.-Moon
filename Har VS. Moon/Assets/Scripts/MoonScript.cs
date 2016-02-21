44debb41aa9aca8ebd0688a88d3bc4737af1facd
Omer Ahmed <omer1@ualberta.ca>
2016-02-21 11:20:12 -0700

    Moon time
    It's moon time. It moves down.

--- Har VS. Moon/Assets/Scripts/MoonScript.cs
+++ Har VS. Moon/Assets/Scripts/MoonScript.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class MoonScript : MonoBehaviour {
+    public WorldManager world;
+    public float currentTime;
+    public float endTime;
+    public GameObject startPosition;
+    public GameObject endPosition;
+    public bool gameEnded = false;
 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        currentTime = world.getTime();
+        if(gameEnded == false)
+        {
+            if(currentTime >= endTime)
+            {
+                gameEnded = true;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(startPosition.transform.position, endPosition.transform.position, currentTime / endTime);
+            }
+        }
 	}
 }
